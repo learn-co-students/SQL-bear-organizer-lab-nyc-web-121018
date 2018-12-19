84fe1ac11fe7b50806ecd15c3228c3498bbe9674
EvanPavley <github email address>
2018-12-19 10:49:36 -0500

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'brown', 'mild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'Female', 'black', 'angry', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 8, 'Male', 'green', 'good', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'Male', 'pink', 'bashful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 20, 'Female', 'red', 'soulful', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'yello', 'regretful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'Female', 'red', 'greasey', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, 'Male', 'brown', 'mild', 1);
